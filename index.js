5c0c08308dd7b7d35e9e5bd4df8849048009ee83
RenanTafner <renantafner@gmail.com>
2023-09-07 23:40:49 -0300

    ajuste descrição metodo de recuperar todas as enquetes swagger

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
  * @swagger
  * /enquetes:
  *   get:
- *     summary: Retorna todos as enquetes cadastradas.
+ *     summary: Retorna todas as enquetes cadastradas.
  *     responses:
  *       200:
  *         description: Lista de enquetes.
